615bce21668097931d874276c98c38a7cfa92e88
Sakshi Gautam <gautamsakshi551@gmail.com>
2025-10-25 17:06:36 +0530

    Add VPC, subnet, and IAM policy attachment resources

---
+++ terraform/dependency.tf
@@ -0,0 +1,14 @@
+resource "aws_vpc" "my_vpc" {
+  cidr_block = "10.0.0.0/16"
+}
+
+resource "aws_subnet" "my_subnet" {
+  vpc_id     = aws_vpc.my_vpc.id
+  cidr_block = "10.0.1.0/24"
+}
+
+resource "aws_iam_role_policy_attachment" "policy_attach" {
+  role       = aws_iam_role.my_role.name
+  policy_arn = aws_iam_policy.my_policy.arn
+  depends_on = [aws_iam_policy.my_policy]
+}
