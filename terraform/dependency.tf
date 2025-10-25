f93b99695b33ec6067325ee1a1576936f03a7e0c
Sakshi Gautam <gautamsakshi551@gmail.com>
2025-10-25 17:19:05 +0530

    Add explicit dependency in IAM policy attachment
    Added explicit dependency for IAM role policy attachment.

--- terraform/dependency.tf
+++ terraform/dependency.tf
@@ -1,3 +1,7 @@
+
+
+Implicit dependency
+
 resource "aws_vpc" "my_vpc" {
   cidr_block = "10.0.0.0/16"
 }
@@ -7,6 +11,7 @@ resource "aws_subnet" "my_subnet" {
   cidr_block = "10.0.1.0/24"
 }
 
+Explicit dependency
 resource "aws_iam_role_policy_attachment" "policy_attach" {
   role       = aws_iam_role.my_role.name
   policy_arn = aws_iam_policy.my_policy.arn
