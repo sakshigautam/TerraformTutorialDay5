39fd8e063ff1943b08960f6d6bc8908aafa11af6
Sakshi Gautam <gautamsakshi551@gmail.com>
2025-10-25 17:05:42 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,1 +1,11 @@
+data "aws_vpc" "existing_vpc" {
+  filter {
+    name   = "tag:Name"
+    values = ["my-existing-vpc"]
+  }
+}
 
+resource "aws_subnet" "example" {
+  vpc_id     = data.aws_vpc.existing_vpc.id
+  cidr_block = "10.0.1.0/24"
+}
